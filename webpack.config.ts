9da941e45a69f060845c6caa09b8142d9abb2a50
Salman <akhmadov.sw@gmail.com>
2022-09-19 13:49:26 +0300

    initial proect

---
+++ webpack.config.ts
@@ -0,0 +1,32 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import { buildPlugins } from "./config/build/buildPlugins";
+import { buildLoaders } from "./config/build/buildLoaders";
+import { buildResolvers } from "./config/build/buildResolvers";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildEnv, BuildPaths } from "./config/build/types/config";
+
+
+
+export default (env: BuildEnv) => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html")
+  }
+  
+  const mode = env.mode || "development";
+  const isDev = mode === "development";
+  const PORT = env.port || 3000;
+  
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  
+  })
+
+  return config
+}
