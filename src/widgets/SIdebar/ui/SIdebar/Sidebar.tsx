2e0ea12719ba2fe85878c702dc52449acf6383d5
Salman <akhmadov.sw@gmail.com>
2022-10-04 15:10:51 +0300

    add react testing library, add tests for btn, sidebar

--- src/widgets/SIdebar/ui/SIdebar/Sidebar.tsx
+++ src/widgets/SIdebar/ui/SIdebar/Sidebar.tsx
@@ -1,29 +1,36 @@
-import { useState } from "react";
-import { classNames } from "shared/lib/classNames/classNames";
-import {LangSwitcher} from "widgets/LangSwitcher/LangSwitcher";
-import { ThemeSwitcher } from "widgets/ThemeSwitcher";
-import cls from "./Sidebar.module.scss";
+import { useState } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import Button from 'shared/ui/Button/Button';
+import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
+import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
     className?: string;
 }
 
-export const Sidebar = ({className}: SidebarProps) => {
-    const [collapsed, setCollapsed] = useState(false)
+export const Sidebar = ({ className }: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false);
 
     const onToggle = () => {
-        setCollapsed(!collapsed)
-    }
+        setCollapsed(!collapsed);
+    };
     return (
-    <div className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [])}>
-        <button onClick={onToggle}>
-            toggle
-        </button>
-        <div className={cls.switchers}>
-            <ThemeSwitcher/>
-            <LangSwitcher className={cls.lang}/>
+        <div
+            data-testid="sidebar"
+            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [])}
+        >
+            <Button
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+            >
+                toggle
+            </Button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher />
+                <LangSwitcher className={cls.lang} />
+            </div>
         </div>
-    </div>
     );
 };
 export default Sidebar;
