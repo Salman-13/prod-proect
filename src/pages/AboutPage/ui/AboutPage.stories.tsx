2c63647dd637edea2364d17f1296c8b7767acfba
Salman <akhmadov.sw@gmail.com>
2022-10-06 18:03:43 +0300

    fix error

--- src/pages/AboutPage/ui/AboutPage.stories.tsx
+++ src/pages/AboutPage/ui/AboutPage.stories.tsx
@@ -13,7 +13,7 @@ export default {
     },
 } as ComponentMeta<typeof AboutPage>;
 
-const Template: ComponentStory<typeof AboutPage> = (args) => <AboutPage {...args} />;
+const Template: ComponentStory<typeof AboutPage> = (args) => <AboutPage {...args as object} />;
 
 export const Light = Template.bind({});
 Light.args = {
--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -2,5 +2,5 @@ import { lazy } from 'react';
 
 export const MainPageAsync = lazy(() => new Promise((resolve) => {
     // @ts-ignore
-    setTimeout(() => resolve(import('./MainPAge')), 1500);
+    setTimeout(() => resolve(import('./MainPage')), 1500);
 }));
