2db8d2fa22e483cd6ea8bb2499d2207058092560
Salman <akhmadov.sw@gmail.com>
2022-10-14 23:13:12 +0300

    add redux store and first entity

---
+++ src/app/providers/StoreProvider/config/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { counterReducer } from 'entities/Counter';
+import { StateSchema } from './StateSchema';
+
+export function createReduxStore(initialState: StateSchema) {
+    return configureStore({
+        reducer: {
+            counter: counterReducer,
+        },
+        devTools: __IS_DEV__,
+        preloadedState: initialState,
+    });
+}
