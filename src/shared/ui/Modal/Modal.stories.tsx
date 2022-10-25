644fb633f33872a0bd00ed4157a2ba214bea42b5
Salman <akhmadov.sw@gmail.com>
2022-10-25 14:14:06 +0300

    fix screen

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Modal } from 'shared/ui/Modal/Modal';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import LoginForm from 'features/AuthByUsername/ui/LoginForm/LoginForm';
-import { Modal } from './Modal';
 
 export default {
     title: 'shared/Modal',
@@ -15,15 +14,15 @@ export default {
 
 const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
 
-export const Light = Template.bind({});
-Light.args = {
+export const Primary = Template.bind({});
+Primary.args = {
     isOpen: true,
-    children: <LoginForm />,
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
 
 export const Dark = Template.bind({});
 Dark.args = {
     isOpen: true,
-    children: <LoginForm />,
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
