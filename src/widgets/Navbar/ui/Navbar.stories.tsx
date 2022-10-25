aeeb530243d09a1a06eed427bea76191ddbbb6a8
Salman <akhmadov.sw@gmail.com>
2022-10-25 14:39:40 +0300

    fix

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -19,7 +19,9 @@ const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
 export const Light = Template.bind({});
 Light.args = {
 };
-Light.decorators = [StoreDecorator({})];
+Light.decorators = [StoreDecorator({
+
+})];
 
 export const Dark = Template.bind({});
 Dark.args = {
