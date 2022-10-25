1ff09abbf29fe28bae663656d4dd541c818f1141
Salman <akhmadov.sw@gmail.com>
2022-10-25 15:14:20 +0300

    fix Navbar stories

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -11,12 +11,12 @@ export default ({ config }: {config: webpack.Configuration}) => {
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
 
-    config.resolve.modules = [paths.src, 'node_modules'];
+    config.resolve.modules?.unshift(paths.src);
 
-    config.resolve?.extensions?.push('ts', 'tsx');
+    config.resolve.extensions.push('ts', 'tsx');
 
     // eslint-disable-next-line no-param-reassign
-    config.module.rules = config.module?.rules?.map((rule: RuleSetRule) => {
+    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return {
                 ...rule, exclude: /\.svg$/i,
@@ -26,16 +26,16 @@ export default ({ config }: {config: webpack.Configuration}) => {
         return rule;
     });
 
-    config.module?.rules?.push(
+    config.module.rules.push(
         {
             test: /\.svg$/,
             use: ['@svgr/webpack'],
         },
     );
 
-    config.module?.rules?.push(buildCssLOader(true));
+    config.module.rules.push(buildCssLOader(true));
 
-    config.plugins?.push(new DefinePlugin({
+    config.plugins.push(new DefinePlugin({
         __IS_DEV__: true,
     }));
 
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,7 @@
 import { getUserAuthData, userActions } from 'entities/User';
 import { LoginModal } from 'features/AuthByUsername';
-import { t } from 'i18next';
 import { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { classNames } from 'shared/lib/classNames/classNames';
 import Button, { ButtonTheme } from 'shared/ui/Button/Button';
@@ -12,6 +12,7 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
     const [isAuthModal, setIsAuthModal] = useState(false);
     const authData = useSelector(getUserAuthData);
     const dispatch = useDispatch();
