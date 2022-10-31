2ce94c6d88d792abd2cb5a0ba83f52e71bd02ef3
Salman <akhmadov.sw@gmail.com>
2022-10-31 22:29:47 +0300

    add tests for login module

---
+++ src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
@@ -0,0 +1,28 @@
+import { AsyncThunkAction } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+
+type ActionCreatorType<Return, Arg, RejectValue>
+= (arg: Arg) => AsyncThunkAction<Return, Arg, {
+    rejectValue: RejectValue;
+}>
+
+export class TestAsyncThunk<Return, Arg, RejectValue> {
+    dispatch: jest.MockedFn<any>;
+
+    getState: () => StateSchema;
+
+    actionCreator: ActionCreatorType<Return, Arg, RejectValue>;
+
+    constructor(actionCreator: ActionCreatorType<Return, Arg, RejectValue>) {
+        this.actionCreator = actionCreator;
+        this.dispatch = jest.fn();
+        this.getState = jest.fn();
+    }
+
+    async callThunk(arg: Arg) {
+        const action = this.actionCreator(arg);
+        const result = await action(this.dispatch, this.getState, undefined);
+
+        return result;
+    }
+}
